dc73beb0cac9cec6bc8834a27d67afca5943329a
AhaLims <915673037@qq.com>
2019-05-19 18:26:27 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,4 +10,24 @@ Page({
     button1Src : "../../imgs/index-button.png",
     button2Src: "../../imgs/index-capsule-jar.png" 
   },
+  onLoad:function(option){
+    var that = this;
+    that.setData(
+      {
+        longitude: 113,
+        latitude: 23,
+        makers:[ 
+          {
+            id : 0,
+            iconPath: "../../imgs/index-capsule-icon-map.png",
+            //其他人的capsule信息 这里先随便填...????
+            longitude: 114,
+            latitude: 23,
+            width: 30,
+            height: 30
+          }
+        ]//等会要换成获取到的用户的位置
+      }
+    )
+  }
 })
